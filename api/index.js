7c7f9841f8a89e661946ce5956b46e9628cf6496
za-xuxuejian <xuxuejian@zhongan.io>
2018-09-10 19:22:19 +0800

    为了实现跨域请求，分离出服务端部分

--- api/index.js
+++ api/index.js
@@ -1,23 +1,45 @@
 import 'whatwg-fetch'
 
+const BASE_URL = 'http://localhost:3002'
 const api = {
-  post(url, {body, ...option}) {
+  post(url, {body = '', ...option}) {
+    if (body) body = JSON.stringify(body)
     const options = {
-      credentials: 'same-origin', // omit include
+      credentials: 'include', // omit include same-origin
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify(body),
+      body,
       ...option,
       method: 'POST'
     }
-    fetch(url, options).then(res => {
+    return fetch(BASE_URL + url, options).then(res => {
       console.log(res)
+      return res.json()
+    })
+  },
+  get(url, option) {
+    const options = {
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      ...option,
+      method: 'GET'
+    }
+    return fetch(BASE_URL + url, options).then(res => {
+      return res.json()
     })
   }
 }
 export default {
   Login(data) {
-    return api.post('/api/login', data)
-  }
+    return api.post(`/api/login`, data)
+  },
+  Register(data) {
+    return api.post(`/api/register`, data)
+  },
+  GetUserInfo() {
+    return api.get(`/api/user`)
+  },
 }
--- components/login/login.js
+++ components/login/login.js
@@ -8,30 +8,57 @@ class login extends Component {
   state = {
     params: {
       account: null,
-      password: null
-    }
+      password: null,
+      code: null
+    },
+    isLogin: true
   }
   login = (e) => {
     e.preventDefault()
+    const {account, password} = this.state.params
     console.log(this.state.params)
-    api.Login({body: this.state.params})
+    api.Login({body: {account, password}}).then(res => {
+      console.log(res)
+    })
+  }
+  register = (e) => {
+    e.preventDefault()
+    const {account, code} = this.state.params
+    console.log(this.state.params)
+    api.Register({body: {account, code}}).then(res => {
+      console.log(res)
+    })
   }
   onChange = (data) => {
     const {params} = this.state
     this.setState({params: {...params, ...data}})
   }
+  changeLogin = () => {
+    this.setState({isLogin: true})
+  }
+  changeRegister = () => {
+    this.setState({isLogin: false})
+  }
+  test = () => {
+    api.GetUserInfo().then(res => {
+      console.log(res)
+    })
+  }
   render() {
     const {className} = this.props
+    const {isLogin} = this.state
     return (
       <div className={`${styles.modal} ${className}`}>
         <div className={styles.header}>
           <h1>
-            <span className={styles.active}>登录</span>
-            <span>注册</span>
+            <span className={styles.active} onClick={this.changeLogin}>登录</span>
+            <span onClick={this.changeRegister}>注册</span>
           </h1>
-          <button className={styles.close}></button>
+          <button className={styles.close} onClick={this.test}></button>
         </div>
         <div className={styles.body}>
+        {
+          isLogin &&
           <form>
             <div>
               <Input placeholder="请输入手机号" name="account" onChange={(e) => this.onChange({account: e.target.value})}/>
@@ -43,6 +70,21 @@ class login extends Component {
             </div>
             <Button type="submit" onClick={this.login}>登录</Button>
           </form>
+        }
+        {
+          !isLogin &&
+          <form>
+            <div>
+              <Input placeholder="请输入手机号" name="account" onChange={(e) => this.onChange({account: e.target.value})}/>
+              <p>请输入正确的手机号</p>
+            </div>
+            <div>
+              <Input placeholder="4位验证码" name="code" onChange={(e) => this.onChange({code: e.target.value})}/>
+              <p>请输入4位验证码！</p>
+            </div>
+            <Button type="submit" onClick={this.register}>注册</Button>
+          </form>
+        }
         </div>
         <div className={styles.footer}></div>
       </div>
